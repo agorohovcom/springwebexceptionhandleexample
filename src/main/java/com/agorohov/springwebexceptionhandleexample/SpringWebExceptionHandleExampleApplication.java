97518648cf3cfb97795ecf8eb4263f579604deb7
agorohov <aleksandr.gorohov@gmail.com>
2024-04-24 14:37:56 +0300

    initial commit

---
+++ src/main/java/com/agorohov/springwebexceptionhandleexample/SpringWebExceptionHandleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.agorohov.springwebexceptionhandleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebExceptionHandleExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebExceptionHandleExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agorohov/springwebexceptionhandleexample/SpringWebExceptionHandleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agorohov.springwebexceptionhandleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExceptionHandleExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
